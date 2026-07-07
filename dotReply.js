6b2aba6da80cfb7a8acbec6390f20e444e81f60b
salmAlzaabi <salmAlzaabi123@gmail.com>
2026-07-07 16:57:00 +0400

    Update dotReply.js

--- dotReply.js
+++ dotReply.js
@@ -3,21 +3,25 @@ const roles = {
   "1501984361258684416": "نورت السيرفر يالشيخ💗."
 };
 
+const users = {
+  "1373005291880316928": "ارحبيي يالامبراطوره☝🏿☝🏿."
+};
+
 export default function registerDotReply(client) {
   client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
     if (message.content.trim() !== ".") return;
-
     const member = message.member;
     if (!member) return;
-
     const replies = [];
+    if (users[message.author.id]) {
+      replies.push(users[message.author.id]);
+    }
     for (const roleId in roles) {
       if (member.roles.cache.has(roleId)) {
         replies.push(roles[roleId]);
       }
     }
-
     if (replies.length > 0) {
       message.reply(replies.join("\n"));
     }
